49ad7cdba1853026db1f962fa4b3374220e57014
nakamurk <1218k.nakamura@gmail.com>
2021-01-23 19:03:16 +0900

    onoue 12 done!

--- react-tutorial/src/api.js
+++ react-tutorial/src/api.js
@@ -1,6 +1,7 @@
 export async function fetchImages(breed) {
+    const dogNumber = 1;
     const response = await fetch(
-        `https://dog.ceo/api/breed/${breed}/images/random/12`
+        `https://dog.ceo/api/breed/${breed}/images/random/${dogNumber}`
     );
     const data = await response.json();
     return data.message;
